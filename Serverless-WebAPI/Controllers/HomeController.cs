bf6197176d755b2b541e8014ce23e7fb86e974a9
venkateshgaddam <venkateshgaddam29@gmail.com>
2019-12-17 16:22:26 +0530

    AppCommit

---
+++ Serverless-WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Serverless_WebAPI.Controllers
+{
+    [Route("aws/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Get")]
+        public string TestApi()
+        {
+            return "Api Testing is Done";
+        }
+    }
+}
